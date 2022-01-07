42eaa44e1d4ef869839b1fc060d06fe2a9947f93
Francine Melo Pêpe <franmelopepe2@gmail.com>
2022-01-07 21:12:05 +0100

    animation lading page

---
+++ photopedia/src/HeaderAnimation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HeaderAnimation.css';
+
+function HeaderAnimation() {
+    return (
+        <>
+            <h1 class="where_to">WHERE TO ?</h1>
+        </>
+    );
+}
+
+export default HeaderAnimation;
